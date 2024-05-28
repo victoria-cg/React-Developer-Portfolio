bbc608a8626ad5fda4c6a5d6a132d3ad66553a7c
Victoria G <125226074+victoria-cg@users.noreply.github.com>
2024-05-27 21:48:40 -0500

    added components for header and nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-//header will import the navigation component into it for the nav button functionality
+//header will import the navigation component into it for the nav button functionality, navigation will have a navbar component for that functionality
+import Navigation from './Navigation.jsx';
+  
+export default function Header(){
+    return(
+        <div>
+            <h1>Victoria Greenwood</h1>
+            <Navigation/> 
+        </div>
+    )
+};
--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,20 @@ import ReactDOM from 'react-dom/client'
 // Import createBrowserRouter and RouterProvider from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-//for commented out code, need to re-activate bootstrap, need to create router, need to make router paths for the real pages in this app
+//import bootstrap library to use for styling
 
-// import 'bootstrap/dist/css/bootstrap.min.css'
+ import 'bootstrap/dist/css/bootstrap.min.css'
 
 //import all the different page components to be able to use them in the router
 
-import App from './App';
+import App from './App.jsx';
 // import Error from './pages/Error';
 // import Home from './pages/Home';
 // import Contact from './pages/Contact';
 // import About from './pages/About';
 // import Blog from './pages/Blog';
 
-// //Create client side react routes and their URL endpoints
+//Create client side react routes and their URL endpoints
 const router = createBrowserRouter([
 //   {
 //     path: '/',
@@ -44,7 +44,7 @@ const router = createBrowserRouter([
 //   },
  ]);
 
-//appends dynamic react content to the html element with and id of 'root'
+//appends dynamic react content to the html element with an id of 'root'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 );
