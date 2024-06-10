6715baf70e094fa6713bdff83f60a5c67ac62abd
Victoria G <125226074+victoria-cg@users.noreply.github.com>
2024-06-10 17:39:58 -0500

    added portfolio component with title placeholder state for each project

---
+++ src/components/Project.jsx
@@ -0,0 +1,19 @@
+
+//project component will receive blogs as props and will get imported/used in the portfolio page to show the projects
+
+//here we iterate over the array in Portfolio.jsx using .map method to print info about every project
+//the key property lets the DOM keep track of each element for updating/outputting it, in this case the unique key is the id of each project
+export default function Project(props) {
+    const projects = props.projects
+    return (
+     <div>
+    {projects.map((project) =>(
+    <div key={project.id}> 
+    <h2>{project.title}</h2>
+    </div>
+    ))}
+     </div>
+    );
+  }
+
+
--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,10 +1,23 @@
+//useState will allow us to use state/data to pass data to child components as props
+import {useState} from 'react';
+import Project from '../Project';
+
+//neet to add URL and photo for each project so the portfolio can be used
+//useState receives the array of projects objects so that we can iterate over them with .map function to print infor about each project 
+//send projects state/data as props to the project component so it can use the data
 export default function Portfolio() {
+  const [projects, setProjects] = useState([
+    {title: 'Project 1', id: 1},
+    {title: 'Project 2', id: 2},
+    {title: 'Project 3', id: 3},
+    {title: 'Project 4', id: 4},
+    {title: 'Project 5', id: 5},
+    {title: 'Project 6', id: 6}
+  ]);
+
     return (
       <div>
-
-        <p>
-        placeholder for this page
-        </p>
+       <Project projects={projects} />
       </div>
     );
   }
